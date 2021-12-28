6e34efdd615c0e2ee9ee0c5cb55a1064f4f300f5
Anatoliy <muzhik.lapotnik@gmail.com>
2021-12-28 22:01:30 +0500

    Урок 5

--- lesson-5/public/script.js
+++ lesson-5/public/script.js
@@ -1,40 +1,4 @@
-const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
-
-function send(onError, onSuccess, url, method = 'GET', data = '', headers = {}, timeout = 60000) {
-
-  let xhr;
-
-  if (window.XMLHttpRequest) {
-    // Chrome, Mozilla, Opera, Safari
-    xhr = new XMLHttpRequest();
-  } else if (window.ActiveXObject) {
-    // Internet Explorer
-    xhr = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-
-  for([key, value] of Object.entries(headers)) {
-    xhr.setRequestHeader(key, value)
-  }
-
-  xhr.timeout = timeout;
-
-  xhr.ontimeout = onError;
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-      if(xhr.status < 400) {
-        onSuccess(xhr.responseText)
-      } else if (xhr.status >= 400) {
-        onError(xhr.status)
-      }
-    }
-  }
-
-  xhr.open(method, url, true);
-
-  xhr.send(data);
-}
-
+const API_URL = 'http://localhost:3000/api/';
 
 function getCounter() {
   let last = 0;
@@ -45,11 +9,11 @@ function getCounter() {
 const stackIDGenrator = getCounter()
 
 class Good {
-  constructor({ id, title, price, img }) {
+  constructor({ id, title, price }) {
     this.id = id;
     this.title = title;
     this.price = price;
-    this.img = img;
+    this.img = `Product_${id}`;
   }
 
   getId() {
@@ -62,6 +26,10 @@ class Good {
 
   getTitle() {
     return this.title;
+  }
+
+  getImg() {
+    return this.img;
   }
 }
 
@@ -87,21 +55,6 @@ class GoodStack {
   getPrice() {
     return this.good.price * this.count
   }
-
-  add() {
-    this.count++;
-    return this.count;
-  }
-
-  remove() {
-    this.count = 0;
-    return this.count;
-  }
-
-  reduce() {
-    this.count--;
-    return this.count;
-  }
 }
 
 class Cart {
@@ -114,135 +67,50 @@ class Cart {
     console.log(err);
   }
 
-  addToServer(good) {
-    send(this._onError, response => {
-      const data = JSON.parse(response);
-
-      if (+data.result === 1) {
-        this.add(good)
-      }
-    }, `${API_URL}addToBasket.json`);
-  }
-
-  delFromServer(good, reduce) {
-    send(this._onError, response => {
-      const data = JSON.parse(response);
-
-      if (+data.result === 1) {
-        this.remove(good, reduce)
-      }
-    }, `${API_URL}deleteFromBasket.json`);
-  }
-
   fetchGoods() {
-    send(this._onError, response => {
-      const data = JSON.parse(response);
-
-      if (data.contents.length > 0) {
-        data.contents.forEach(({ id_product, product_name, price, quality }) => this.add({
-          id: id_product,
-          title: product_name,
-          price: price,
-          quality: quality,
-          img: `Product_${id_product}`
-        }, quality));
-
-      }
-      this.drawCart.reDrawCart(this);
-    }, `${API_URL}getBasket.json`);
+    return fetch(`${API_URL}cart`)
+      .then(response => { return response.json(); })
+      .then(response => {
+        this.list = [];
+        response.forEach(({id, title, price, count}) => {
+          this.list.push(new GoodStack({ id: id, title: title, price: price, img: `Product_${id}` }, count))
+        });
+        this.drawCart.reDrawCart(this);
+        return response;
+      })
+      .catch(error => this._onError(error));
   }
 
-  add(good, quality) {
-    const idx = this.list.findIndex(stack => stack.getGoodId() == good.id);
+  send(method, data) {
+    return fetch(`${API_URL}cart`, {
+      method: method,
+      body: JSON.stringify(data),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    .then(response => { return response.json(); })
+    .then(response => {
+      if (response.success) {
+        this.fetchGoods()
+      } else {
+        this._onError(response.error)
+      }
+    }).catch(error => this._onError(error));
+  }
 
-    if (idx >= 0) {
-      this.list[idx].add()
-    } else {
-      this.list.push(new GoodStack(good, quality))
+  add(id, count) {
+    if (!id) {
+      return;
     }
-    this.drawCart.reDrawCart(this);
+
+    this.send('POST', { id: id, count: count || 1 })
   }
 
   remove(id, reduce = false) {
-    const idx = this.list.findIndex(stack => stack.getGoodId() == id);
-
-    if (idx >= 0) {
-      if (reduce) {
-        this.list[idx].reduce();
-      } else {
-        this.list[idx].remove();
-      }
-
-      if (this.list[idx].getCount() <= 0) {
-        this.list.splice(idx, 1)
-      }
+    if (!id) {
+      return;
     }
-    this.drawCart.reDrawCart(this);
-  }
-}
 
-class Showcase {
-  constructor(cart){
-    this.list = [];
-    this.cart = cart;
-    this.catalog = new RenderCatalog(this.list, this.cart);
-  }
-
-  _onSuccess(response) {
-    const data = JSON.parse(response)
-    data.forEach(product => {
-      this.list.push(
-        new Good({
-          id: product.id_product,
-          title: product.product_name,
-          price: product.price,
-          img: `product_${product.id_product}`
-        })
-      )
-      this.catalog.drawToCatalog();
-    });
-  }
-
-  _onError(err) {
-    console.log(err);
-  }
-
-  fetchGoods() {
-    send(this._onError, this._onSuccess.bind(this), `${API_URL}catalogData.json`);
-  }
-
-  addToCart(id) {
-    const idx = this.list.findIndex(good => id == good.id)
-
-    if(idx >= 0) {
-      this.cart.addToServer(this.list[idx])
-    }
-  }
-}
-
-class RenderCartProduct {
-  constructor({ count, good: { id, price, title }}){
-    this.id = id;
-    this.count = count;
-    this.price = price;
-    this.title = title;
-  }
-
-  renderGoodsItem() {
-    return `
-      <tr class='card-menu__row' data-id='${this.id}'>
-        <td class='card-menu__col'>${this.title}</td>
-        <td class='card-menu__col'>
-            <span data-id='${this.id}' class="decrease"></span>
-            <span class="count">${this.count}</span>
-            <span data-id='${this.id}' class="increase"></span>
-        </td>
-        <td class='card-menu__col'>${this.price}</td>
-        <td class='card-menu__col'>${this.count * this.price}</td>
-        <td class='card-menu__col'>
-            <span data-id='${this.id}' class='close-btn'></span>
-        </td>
-    `;
+    this.send('DELETE', { id: id, reduce: reduce ? 1 : 0 })
   }
 }
 
@@ -270,13 +138,16 @@ class RenderCart {
 
   drawToCart() {
     this.cartEl.innerHTML = this.goodsList;
-    this.totalPrice.innerHTML = this.total.totalPrice;
+    this.totalPrice.innerHTML = this.total.totalPrice.toFixed(2);
     this.totalCount.innerHTML = this.total.totalCount;
   }
 
   countTotal() {
     this.total = this.cartList.reduce(({ totalCount, totalPrice }, { count, good: { price } }) => {
-      return { totalCount: (count ||= 1) + totalCount, totalPrice: (count ||= 1) * price + totalPrice };
+      return {
+        totalCount: (count ||= 1) + totalCount,
+        totalPrice: (count ||= 1) * price + totalPrice
+      };
     }, { totalCount: 0, totalPrice: 0});
   }
 
@@ -287,6 +158,97 @@ class RenderCart {
     this.renderGoodsList();
     this.countTotal()
     this.drawToCart();
+  }
+}
+
+class RenderCartProduct {
+  constructor({ count, good: { id, price, title }}){
+    this.id = id;
+    this.count = count;
+    this.price = price;
+    this.title = title;
+  }
+
+  renderGoodsItem() {
+    return `
+      <tr class='card-menu__row' data-id='${this.id}'>
+        <td class='card-menu__col'>${this.title}</td>
+        <td class='card-menu__col'>
+            <span data-id='${this.id}' class="decrease"></span>
+            <span class="count">${this.count}</span>
+            <span data-id='${this.id}' class="increase"></span>
+        </td>
+        <td class='card-menu__col'>${this.price}</td>
+        <td class='card-menu__col'>${(this.count * this.price).toFixed(2)}</td>
+        <td class='card-menu__col'>
+            <span data-id='${this.id}' class='close-btn'></span>
+        </td>
+    `;
+  }
+}
+
+class Showcase {
+  constructor(cart){
+    this.list = [];
+    this.cart = cart;
+    this.catalog = new RenderCatalog(this.list);
+  }
+
+  _onSuccess(response) {
+    response.forEach(product => {
+      this.list.push(new Good(product));
+      this.catalog.drawToCatalog();
+    });
+  }
+
+  _onError(error) {
+    console.log(error);
+  }
+
+  fetchGoods() {
+    return fetch(`${API_URL}catalog`)
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        this._onSuccess(response);
+        return response;
+      })
+      .catch(error => this._onError(error));
+  }
+
+  addToCart(id) {
+    const idx = this.list.findIndex(good => id == good.id)
+
+    if(idx >= 0) {
+      this.cart.add(id)
+    }
+  }
+}
+
+class RenderCatalog {
+  constructor(list) {
+    this.list = list;
+    this.goodsList = '';
+    this.productList = document.querySelector('.products__list');
+  }
+
+  renderGoodsList() {
+    this.goodsList = this.list.map(item => {
+        return (new RenderCatalogProduct(item)).renderGoodsItem();
+    }).join('')
+  }
+
+  clearCatalog(){
+    if (this.productList && this.productList.children) {
+      [...this.productList.children].forEach(el => el.remove());
+    }
+  }
+
+  drawToCatalog() {
+    this.clearCatalog();
+    this.renderGoodsList();
+    this.productList.innerHTML = this.goodsList;
   }
 }
 
@@ -301,44 +263,17 @@ class RenderCatalogProduct {
   renderGoodsItem() {
     return `
       <li class="products__item product" data-id="${this.id}">
-					<a class="products__link" href="product.html">
-						<img class="product__img" src="img/${this.img}.jpg" alt="product">
-						<div class="product__desc">
-							<h3 class="product__title">${this.title}</h3>
-							<p class="product__text">Known for her sculptural takes on traditional tailoring, Australian
-								arbiter of cool Kym Ellery teams up with Moda Operandi.</p>
-							<span class="product__price">${this.price}</span>
-						</div>
-					</a>
-				</li>
+          <a class="products__link" href="product.html">
+            <img class="product__img" src="img/${this.img}.jpg" alt="${this.title}">
+            <div class="product__desc">
+              <h3 class="product__title">${this.title}</h3>
+              <p class="product__text">Known for her sculptural takes on traditional tailoring, Australian
+                arbiter of cool Kym Ellery teams up with Moda Operandi.</p>
+              <span class="product__price">${this.price}</span>
+            </div>
+          </a>
+        </li>
     `;
-  }
-}
-
-class RenderCatalog {
-  constructor(list) {
-    this.list = list;
-    this.goodsList = '';
-    this.productList = document.querySelector('.products__list');
-  }
-
-  renderGoodsList() {
-    this.goodsList = this.list.map(item => {
-        return (new RenderCatalogProduct(item)).renderGoodsItem();
-      }
-    ).join('')
-  }
-
-  clearCatalog(){
-    if (this.productList && this.productList.children) {
-      [...this.productList.children].forEach(el => el.remove());
-    }
-  }
-
-  drawToCatalog() {
-    this.clearCatalog();
-    this.renderGoodsList();
-    this.productList.innerHTML = this.goodsList;
   }
 }
 
@@ -363,13 +298,13 @@ productList.addEventListener('click', e => {
 cartEl.addEventListener('click', e => {
   e.preventDefault();
   if (e.target.classList.contains('close-btn') && e.target.dataset.id) {
-    this.cart.delFromServer(e.target.dataset.id);
+    cart.remove(e.target.dataset.id);
   }
   if (e.target.classList.contains('increase') && e.target.dataset.id) {
     showcase.addToCart(e.target.dataset.id);
   }
   if (e.target.classList.contains('decrease') && e.target.dataset.id) {
-    this.cart.delFromServer(e.target.dataset.id, true);
+    cart.remove(e.target.dataset.id, true);
   }
 
 });
